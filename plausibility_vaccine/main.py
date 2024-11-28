f9bf5ce74555bd022baff4485aef9d4af2e6af03
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2024-11-28 11:42:08 -0500

    Run ruff formatting

--- plausibility_vaccine/main.py
+++ plausibility_vaccine/main.py
@@ -2,14 +2,14 @@ def hello_torch() -> str:
     import torch
 
     _ = torch.zeros(1337)
-    return "Hello PyTorch"
+    return 'Hello PyTorch'
 
 
 def main() -> None:
-    print("Checking packages...", end="")
+    print('Checking packages...', end='')
     hello_torch()
-    print("Ok.")
+    print('Ok.')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- test/test_main.py
+++ test/test_main.py
@@ -2,4 +2,4 @@ from plausibility_vaccine.main import hello_torch
 
 
 def test_hello_torch() -> None:
-    assert hello_torch() == "Hello PyTorch"
+    assert hello_torch() == 'Hello PyTorch'
