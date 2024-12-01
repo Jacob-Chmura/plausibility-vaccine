Fix selectional preference calculation

scripts/generate_selectional_association_data.py
@@ -52,21 +52,21 @@ def compute_selectional_association(df: pd.DataFrame, col_name: str) -> pd.DataF
         on=col_name,
     ).rename({'proportion_x': 'p_given_v', 'proportion_y': 'p'}, axis=1)
 
-    def _entropy(p_given_v: np.ndarray, p: np.ndarray) -> float:
-        return -sum((p_given_v * np.log(p_given_v)) / p)
+    def _sr(p_given_v: np.ndarray, p: np.ndarray) -> float:
+        return sum(p_given_v * np.log(p_given_v / p))
 
-    entropy_data = (
+    sr_data = (
         data.groupby('verb')
-        .apply(lambda x: _entropy(x['p_given_v'], x['p']), include_groups=False)
+        .apply(lambda x: _sr(x['p_given_v'], x['p']), include_groups=False)
         .reset_index()
-        .rename({0: 'h'}, axis=1)
+        .rename({0: 'selectional_preference'}, axis=1)
     )
 
-    data = pd.merge(data, entropy_data, how='left', on='verb')
-    data = data[data['h'] > 0]
+    data = pd.merge(data, sr_data, how='left', on='verb')
+    data = data[data['selectional_preference'] > 0]
 
-    data['association'] = data['p_given_v'] * np.log(data['p_given_v']) / data['p']
-    data['association'] /= data['h']
+    data['association'] = data['p_given_v'] * np.log(data['p_given_v'] / data['p'])
+    data['association'] /= data['selectional_preference']
     return data[['verb', col_name, 'association']]
 
 