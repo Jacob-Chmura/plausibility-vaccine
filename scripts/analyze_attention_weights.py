438837b8527546d4e3ef5a001137235638f79f9b
Sebastian C. Sabry <sebastiancsabry@system76-pc.localdomain>
2024-12-08 16:30:07 -0500

    added histogram plots, updated to use pathlib

--- scripts/analyze_attention_weights.py
+++ scripts/analyze_attention_weights.py
@@ -1,5 +1,6 @@
 import argparse
 import pathlib
+from typing import Dict
 
 import matplotlib.pyplot as plt
 import torch
@@ -24,6 +25,62 @@ parser.add_argument(
     help='Path to binary weight file.',
 )
 
+parser.add_argument(
+    '--save-fig-dir',
+    type=str,
+    default='../figs/adapter_attention_weights/',
+    help='Path to saved figure folder.',
+)
+parser.add_argument(
+    '--chart-fig-dir',
+    type=str,
+    default='../figs/adapter_attention_weights/charts_attention_weights/',
+    help='Path to saved attention weight charts.',
+)
+parser.add_argument(
+    '--hist-fig-dir',
+    type=str,
+    default='../figs/adapter_attention_weights/hist_combined_weights/',
+    help='Path to saved attention weight charts.',
+)
+
+
+def plot_weight_distributions(
+    adapter_dict: Dict, task_name: str, save_path: pathlib.Path
+) -> None:
+    for name, _ in adapter_dict.items():
+        if 'weight' in name:  # Ignore bias for now
+            weights_np = adapter_dict[name].detach().cpu().numpy()
+            if weights_np.shape == (len(weights_np),):
+                weights_np = weights_np.reshape(
+                    (len(weights_np), 1)
+                )  # Column array needed for image format
+            print(f'{task_name}')
+            plt.figure(figsize=(10, 6))
+            plt.imshow(weights_np, cmap='viridis', aspect='auto')
+            plt.colorbar()
+            plt.title(f'Dense Layer Weights: {task_name}')
+            plt.savefig(save_path)
+            plt.close()
+
+
+def plot_combined_weight_hist_distributions(
+    adapter_dict: Dict, task_name: str, save_path: pathlib.Path
+) -> None:
+    plt.figure(figsize=(12, 6))
+    all_weights = []
+    for name, _ in adapter_dict.items():
+        weights_np = adapter_dict[name].detach().cpu().numpy().flatten()
+        all_weights.extend(weights_np)
+
+    plt.hist(all_weights, bins=100, alpha=0.5, label=f'{task_name}', density=True)
+    plt.title(f'Combined Weight Distributions: {task_name}')
+    plt.xlabel('Weight Value')
+    plt.ylabel('Density')
+    plt.grid(True)
+    plt.savefig(save_path)
+    plt.close()
+
 
 def main() -> None:
     args = parser.parse_args()
@@ -31,21 +88,20 @@ def main() -> None:
     _, _, _, finetuning_args = parse_args(config_file_path)
     for task_name, _ in finetuning_args.pretraining_tasks.items():
         weight_path = (
-            pathlib.Path(args.weights_dir) / f'{task_name}' / 'pytorch_adapter.bin'
+            pathlib.Path(args.weights_dir) / f'{task_name}' / 'pytorch_model_head.bin'
         )
-        state_dict = torch.load(
+        save_path_charts = (
+            pathlib.Path(args.chart_fig_dir) / f'{task_name}_attention_weights.png'
+        )
+        save_path_hist = (
+            pathlib.Path(args.hist_fig_dir) / f'{task_name}_combined_weights.png'
+        )
+        adapter_dict = torch.load(
             weight_path, map_location=torch.device('cpu'), weights_only=True
         )
+        plot_weight_distributions(adapter_dict, task_name, save_path_charts)
 
-        for name, param in state_dict.items():
-            if 'weight' in name:  # Ignore bias for now
-                print(f'{name}: {param.shape}')
-                weights_np = state_dict[name].detach().cpu().numpy()
-                plt.figure(figsize=(10, 6))
-                plt.imshow(weights_np, cmap='viridis', aspect='auto')
-                plt.colorbar()
-                plt.title('Dense Layer Weights')
-                plt.show()
+        plot_combined_weight_hist_distributions(adapter_dict, task_name, save_path_hist)
 
 
 if __name__ == '__main__':
