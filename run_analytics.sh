a489ecb665e490f044592e91028b4877bc936bf4
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2024-12-09 16:44:36 -0500

    Create base script for running analytics

---
+++ run_analytics.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -euo pipefail
+
+print_usage() {
+    echo "Usage: ${0} [-h]"
+    echo
+    echo "Run Plausibility Vaccine analytics"
+    echo
+    echo "Positional argument:"
+    echo
+    echo "Optional arguments:"
+    echo "  --no-results           Skip analytics that require having previously run Plausibility Vaccine."
+    echo "  -h, --help             Show this help message and exit."
+}
+
+
+RESULTS=1
+
+main() {
+    parse_args "${@}"
+    echo "Running Plausibility Vaccine Analytics..."
+
+    if [[ ${RESULTS} -eq 0 ]]; then
+        echo "Skipping analytics that require having previously run Plausibility Vaccine!"
+    fi
+
+}
+
+parse_args() {
+    local i=0
+
+    while [[ "${#}" -gt 0 ]]; do
+        case "${1}" in
+            -h|--help|help)
+                print_usage
+                exit 0
+                ;;
+            --no-results)
+                RESULTS=0
+                shift
+                ;;
+            *)
+                shift
+        esac
+    done
+
+    return 0
+}
+
+
+main "${@}"
