20564afc940b23d5edf1f29c1f4b34cad191d284
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2024-12-10 11:56:08 -0500

    Implement dataset statistics artifact tables

--- scripts/run_data_analysis.py
+++ scripts/run_data_analysis.py
@@ -1,6 +1,9 @@
 import argparse
+import pathlib
+from typing import Dict
 
 import pandas as pd
+from tabulate import tabulate
 
 from plausibility_vaccine.util.path import get_root_dir
 
@@ -28,23 +31,35 @@ parser.add_argument(
     default='data/verb_understanding_data/',
     help='Path to root directory containing verb data',
 )
+parser.add_argument(
+    '--artifacts-dir',
+    type=str,
+    default='artifacts/dataset_statistics',
+    help='Path to artifact directory containing plots',
+)
 
 
 def main() -> None:
     args = parser.parse_args()
-    analyze_plausibility_datasets(args.plausibility_datasets_dir)
-    analyze_property_datasets(args.property_datasets_dir)
-    analyze_verb_datasets(args.verb_datasets_dir)
+    plausibility_tables = generate_plausibility_tables(args.plausibility_datasets_dir)
+    property_tables = generate_property_tables(args.property_datasets_dir)
+    verb_tables = generate_verb_tables(args.verb_datasets_dir)
+
+    latex_tables = plausibility_tables | property_tables | verb_tables  # type: ignore
+    save_latex_tables(latex_tables, args.artifacts_dir)
 
 
-def analyze_plausibility_datasets(datasets_dir: str) -> None:
+def generate_plausibility_tables(datasets_dir: str) -> Dict[str, str]:
     df = _read_subdirectory_dataset_csvs(datasets_dir)
-    print('------------- Plausibility Data -------------')
-    print(df.groupby(['task', 'split'])['label'].value_counts(normalize=True).round(3))
-    print()
+    df = df.groupby(['task', 'split'])['label'].value_counts(normalize=True)
+    table_data = df.round(3).reset_index()
+
+    headers = ['Task', 'Split', 'Plausibility Label', 'Proportion']
+    print(tabulate(table_data, headers, tablefmt='fancy_grid'))
+    return {'plausibility_data': tabulate(table_data, headers, tablefmt='latex')}
 
 
-def analyze_property_datasets(datasets_dir: str) -> None:
+def generate_property_tables(datasets_dir: str) -> Dict[str, str]:
     PROPERTY_LABEL_BINS = {
         'Mobility': ['rock', 'tree', 'mushroom', 'animal', 'man', 'car'],
         'Opacity': ['glass', 'paper', 'cloud', 'man', 'wall'],
@@ -58,7 +73,7 @@ def analyze_property_datasets(datasets_dir: str) -> None:
         'Weight': ['watch', 'book', 'man', 'jeep', 'stadium'],
     }
 
-    print('------------- Property Data -------------')
+    tables = {}
     df = _read_subdirectory_dataset_csvs(datasets_dir)
     for task_name, df_task in df.groupby('task'):
         df_task = (
@@ -70,23 +85,38 @@ def analyze_property_datasets(datasets_dir: str) -> None:
         df_task['label'] = df_task['label'].apply(
             lambda x: PROPERTY_LABEL_BINS[task_name][x - 1]
         )
-        print(df_task.round(3))
-        print()
+        table_data = df_task.round(3).reset_index()[['task', 'label', 'proportion']]
+
+        headers = ['Task', 'Bin Label', 'Proportion']
+        print(tabulate(table_data, headers, tablefmt='fancy_grid'))
+        tables[f'{task_name}_data'] = tabulate(table_data, headers, tablefmt='latex')
+    return tables
 
 
-def analyze_verb_datasets(datasets_dir: str) -> None:
+def generate_verb_tables(datasets_dir: str) -> Dict[str, str]:
     df = _read_subdirectory_dataset_csvs(datasets_dir)
+    percentiles = [0.01, 0.1, 0.25, 0.5, 0.75, 0.9, 0.99]
 
-    print('------------- Verb Data -------------')
-    for _, df_task in df.groupby('task'):
+    tables = {}
+    for task_name, df_task in df.groupby('task'):
         df_task = df_task.dropna(axis='columns')
-        print(
-            df_task.groupby('task')['label']
-            .describe(percentiles=[0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95])
-            .reset_index()
-            .round(3)
-        )
-        print()
+        df_task = df_task.groupby('task')['label'].describe(percentiles=percentiles)
+        table_data = df_task.round(3).reset_index()
+        headers = ['Task', 'Count', 'Mean', 'Std', 'Min']
+        headers += [f'{int(100*p)}%' for p in percentiles]
+        headers += ['Max']
+        print(tabulate(table_data, headers, tablefmt='fancy_grid'))
+        tables[f'{task_name}_data'] = tabulate(table_data, headers, tablefmt='latex')
+    return tables
+
+
+def save_latex_tables(latex_tables: Dict[str, str], artifacts_dir_str: str) -> None:
+    artifacts_dir = pathlib.Path(artifacts_dir_str)
+    artifacts_dir.mkdir(parents=True, exist_ok=True)
+
+    for table_name, table_latex in latex_tables.items():
+        with open(artifacts_dir / f'{table_name}_statistics.txt', 'w') as f:
+            f.write(table_latex)
 
 
 def _read_subdirectory_dataset_csvs(datasets_dir_str: str) -> pd.DataFrame:
